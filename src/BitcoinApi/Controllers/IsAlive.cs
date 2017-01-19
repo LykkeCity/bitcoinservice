2e5a45bb5dc229abfc5dc8705a859c944b77a0e9
Roman Chuprikov <paradise.develop@gmail.com>
2017-01-19 22:20:38 +0500

    add is alive api

---
+++ src/BitcoinApi/Controllers/IsAlive.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BitcoinApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class IsAlive : Controller
+    {
+        [HttpGet]
+        public IsAliveResponse Get()
+        {
+            return new IsAliveResponse
+            {
+                Version =
+                    Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationVersion
+            };
+        }
+
+        public class IsAliveResponse
+        {
+            public string Version { get; set; }
+        }
+    }
+}
