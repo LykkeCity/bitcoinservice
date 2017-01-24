794b5ecb1e430523355683d4ed9f9b67ff89528f
Roman Chuprikov <paradise.develop@gmail.com>
2017-01-24 15:34:29 +0500

    add script for poison messages reenqueue

---
+++ src/PoisonMessagesReenqueue/DependencyBinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AzureRepositories.Walelts;
+using AzureStorage.Queue;
+using AzureStorage.Tables;
+using Core.Repositories.Wallets;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NBitcoin.BuilderExtensions;
+
+namespace PoisonMessagesReenqueue
+{
+    public class DependencyBinder
+    {
+        public static IServiceProvider BindAndBuild(IConfigurationRoot configuration)
+        {
+            var collection = new ServiceCollection();
+
+            collection.AddTransient<ReenqueueJob>();
+
+            collection.AddTransient<Func<string, IQueueExt>>(x => queueName => new AzureQueueExt(configuration.GetConnectionString("job"), queueName));
+
+            collection.AddSingleton<IConfiguration>(configuration);
+
+            return collection.BuildServiceProvider();
+        }
+    }
+}
