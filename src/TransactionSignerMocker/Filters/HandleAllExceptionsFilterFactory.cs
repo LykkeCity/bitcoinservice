cf18dc185970dd1f6e6cf5e7d729240d6837ce71
bijakatlykkex <bijakatlykkex@gmail.com>
2017-01-24 12:43:52 +0330

    Initial bitcoin api mock. Functions not still developped.

---
+++ src/TransactionSignerMocker/Filters/HandleAllExceptionsFilterFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Log;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace TransactionSignerMocker.Filters
+{
+    public class HandleAllExceptionsFilterFactory : IFilterFactory
+    {
+        public bool IsReusable => true;
+
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+        {
+            var log = (ILog)serviceProvider.GetService(typeof(ILog));
+            return new HandleAllExceptionsFilter(log);
+        }
+    }
+}
