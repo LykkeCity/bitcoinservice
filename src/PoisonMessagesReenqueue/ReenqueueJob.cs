bf4b69dab62be42e6d4ab711e4476ed5bedc76d6
Roman Chuprikov <paradise.develop@gmail.com>
2017-01-24 15:34:29 +0500

    add script for poison messages reenqueue

---
+++ src/PoisonMessagesReenqueue/ReenqueueJob.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureStorage.Queue;
+using Microsoft.Extensions.Configuration;
+
+namespace PoisonMessagesReenqueue
+{
+    public class ReenqueueJob
+    {
+        private readonly IQueueExt _fromQueue;
+        private readonly IQueueExt _toQueue;
+
+        public ReenqueueJob(Func<string, IQueueExt> queueFactory, IConfiguration configuration)
+        {
+            var queueName = configuration.GetValue<string>("QueueName");
+            _fromQueue = queueFactory(queueName + "-poison");
+            _toQueue = queueFactory(queueName);
+        }
+
+        public async Task Start()
+        {
+            while (await _fromQueue.Count() > 0)
+            {
+                var msg = await _fromQueue.GetRawMessageAsync();
+                await _toQueue.PutRawMessageAsync(msg.AsString);
+                await _fromQueue.FinishRawMessageAsync(msg);
+            }
+        }
+    }
+}
