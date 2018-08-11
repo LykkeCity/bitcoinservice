38b84c503b8cf4771440556dd8ac958f0fece025
Mikhail Musin <mikhail.musin@gmail.com>
2018-08-11 21:02:57 +0300

    LWDEV-8893 cashin/cashout events

--- src/Lykke.Bitcoin.Contracts/Events/CashinCompletedEvent.cs
+++ src/Lykke.Bitcoin.Contracts/Events/CashinCompletedEvent.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MessagePack;
+using System;
 
 namespace Lykke.Bitcoin.Contracts.Events
 {
+    [MessagePackObject(keyAsPropertyName: true)]
     public class CashinCompletedEvent
     {
         public Guid OperationId { get; set; }
